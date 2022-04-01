1433403afb7eeb4aa4ec99241c051231d3ae630d
Afeef001 <coder.afeef@gmail.com>
2022-04-02 02:09:58 +0600

    Initial commit

---
+++ Turtle Graphics/turtle1.py
@@ -0,0 +1,21 @@
+# A circle pattern with triangles
+
+import turtle
+
+turtle.speed(0)
+turtle.hideturtle()
+turtle.bgcolor("Black")
+turtle.color("blue","skyblue")
+# turtle.begin_fill()
+
+
+for i in range(12*2):
+
+	turtle.forward(100)
+	turtle.left(30)
+	turtle.forward(100)
+	turtle.left(165)
+	turtle.forward(195)
+# turtle.end_fill()
+
+turtle.done()
