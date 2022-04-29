d7f9dc7952d6bd4a69ab5f4457d84765bc22e67c
Afeef001 <coder.afeef@gmail.com>
2022-04-29 16:49:05 +0600

    Random Practice

---
+++ Turtle Graphics/Turtle 2(1).py
@@ -0,0 +1,53 @@
+# A circle pattern with triangles
+
+import turtle
+
+turtle.speed(5)
+turtle.hideturtle()
+turtle.bgcolor("Black")
+turtle.color("Lime","Lime")
+# turtle.begin_fill()
+
+
+for i in range(12*2):
+
+	turtle.forward(100)
+
+	turtle.left(30)
+	turtle.forward(100)
+	turtle.right(130)
+	turtle.forward(150)
+# turtle.end_fill()
+
+turtle.done()
+
+
+
+
+
+
+
+
+# A circle pattern with triangles
+#Genius Work!!!
+
+# import turtle
+
+# turtle.speed(9)
+# # turtle.hideturtle()
+# turtle.bgcolor("Black")
+# turtle.color("Lime","Lime")
+# # turtle.begin_fill()
+
+
+# for i in range(12*2):
+
+# 	turtle.forward(100)
+
+# 	turtle.left(30)
+# 	turtle.forward(100)
+# 	turtle.right(165)
+# 	turtle.forward(195)
+# # turtle.end_fill()
+
+# turtle.done()
